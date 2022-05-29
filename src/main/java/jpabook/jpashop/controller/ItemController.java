c878cfa1d728a8580e023a360a5faabdf56bbb4c
Subin Cho <sbjo7713@gmail.com>
2022-05-30 05:28:03 +0900

    아이템 수정 Controller, View 구현

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -5,15 +5,19 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.form.BookForm;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ItemController {
 
     private final ItemService itemService;
@@ -44,6 +48,39 @@ public class ItemController {
         model.addAttribute("items", items);
         return "/items/itemList";
 
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable Long itemId, Model model) {
+        Book findItem = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(findItem.getId());
+        form.setName(findItem.getName());
+        form.setPrice(findItem.getPrice());
+        form.setStockQuantity(findItem.getStockQuantity());
+        form.setAuthor(findItem.getAuthor());
+        form.setIsbn(findItem.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form, @PathVariable String itemId) {
+
+        Book book = new Book();
+
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+
+        return "redirect:/items";
     }
 
 }
--- src/main/java/jpabook/jpashop/form/BookForm.java
+++ src/main/java/jpabook/jpashop/form/BookForm.java
@@ -6,14 +6,10 @@ import lombok.Setter;
 @Getter @Setter
 public class BookForm {
 
-    // 상품의 공통 속성
     private Long id;
-    
     private String name;
     private int price;
     private int stockQuantity;
-
-    // BOOK 의 속성
     private String author;
     private String isbn;
 
