af0e9a07a3f4104d8bcb782e40bbefa02fe5e7d5
Subin Cho <sbjo7713@gmail.com>
2022-05-30 01:50:09 +0900

    멤버 생성 Controller, View 생성

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.dto.MemberForm;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/members")
+@RequiredArgsConstructor
+@Slf4j
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/new")
+    public String createMember(@Valid @ModelAttribute MemberForm memberForm) {
+
+        Member member = memberForm.toEntity();
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+}
---
+++ src/main/java/jpabook/jpashop/dto/MemberForm.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.dto;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.embedded.Address;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class MemberForm {
+
+    @NotEmpty(message = "회원 이름은 필수 입니다.")
+    private String name;
+
+    private String city;
+
+    private String street;
+
+    private String zipcode;
+
+    public Member toEntity() {
+        Member member = new Member();
+        Address address = new Address(this.getCity(), this.getStreet(), this.getZipcode());
+        member.setName(this.getName());
+        member.setAddress(address);
+        return member;
+    }
+
+}
