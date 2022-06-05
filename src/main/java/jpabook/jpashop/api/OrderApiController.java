cf1d643600e0b3c97b0fb2cabb5ed7d239d710ed
Subin Cho <sbjo7713@gmail.com>
2022-06-06 03:51:18 +0900

    주문 조회 API (엔티티 직접 노출) 방식 구현

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.dto.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderService orderService;
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            order.getOrderItems().stream()
+                .forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+
+}
