2d96c960eb35515689295e57163ced2c984d8a2b
Subin Cho <sbjo7713@gmail.com>
2022-06-07 19:39:03 +0900

    DTO로 직접 조회, 플랫 데이터

--- src/main/java/jpabook/jpashop/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/query/OrderQueryDto.java
@@ -3,11 +3,13 @@ package jpabook.jpashop.query;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.embedded.Address;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
 
     public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
@@ -16,6 +18,15 @@ public class OrderQueryDto {
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 
     private Long orderId;
